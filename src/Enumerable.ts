2cf4200c6fcac24e5de4747ff687762326f27477
Robert Herber <rwherber@gmail.com>
2021-09-08 08:48:22 -0700

    docs: add more docs

--- src/Enumerable.ts
+++ src/Enumerable.ts
@@ -245,6 +245,37 @@ export class Enumerable<TSource> implements Iterable<TSource> {
 
   /**
    * Correlates the elements of two sequences based on key equality, and groups the results.
+   * ``` typescript
+   * const magnus = { name: 'Magnus' };
+   * const terry = { name: 'Terry' };
+   * const adam = { name: 'Adam' };
+   *
+   * const barley = { name: 'Barley', owner: terry };
+   * const boots = { name: 'Boots', owner: terry };
+   * const whiskers = { name: 'Whiskers', owner: adam };
+   * const daisy = { name: 'Daisy', owner: magnus };
+   *
+   * const people = from([magnus, terry, adam]);
+   * const pets = from([barley, boots, whiskers, daisy]);
+   *
+   * const result = people
+   *   .groupJoin(
+   *     pets,
+   *     person => person,
+   *     pet => pet.owner,
+   *     (person, petCollection) => ({ ownerName: person.name, pets: petCollection.select(p => p.name).toArray() })
+   *   )
+   *   .toArray();
+   *
+   * expect(result).toEqual([
+   *   { ownerName: 'Magnus', pets: ['Daisy'] },
+   *   { ownerName: 'Terry', pets: ['Barley', 'Boots'] },
+   *   { ownerName: 'Adam', pets: ['Whiskers'] }
+   * ]);
+   * ```
+   * @typeparam TInner The type of the elements of the second sequence.
+   * @typeparam TKey The type of the keys returned by the key selector functions.
+   * @typeparam TResult The type of the result elements.
    * @param inner The sequence to join to the first sequence.
    * @param outerKeySelector A function to extract the join key from each element of the first sequence.
    * @param innerKeySelector A function to extract the join key from each element of the second sequence.
